2fe2e8a45498d0f88951eeba41af4fce8a6fc821
arnaud <arnaudbenede@hotmail.fr>
2020-05-27 14:47:22 +0200

    connexion fb and google

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+
+import FacebookLogin from 'react-facebook-login';
+import { GoogleLogin } from 'react-google-login';
+
 import './App.css';
 
 function App() {
+  const responseFacebook = (response) => {
+    console.log('response Facebook', response);
+  }
+
+  const componentClicked = () => {
+    console.log('clicked');
+  }
+
+  const responseGoogle = (response) => {
+  console.log('response Google ', response);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FacebookLogin
+        appId="1088597931155576"
+        autoLoad={true}
+        fields="name,email,picture"
+        onClick={componentClicked}
+        callback={responseFacebook}
+        />
+        <br/>
+        <GoogleLogin
+          clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={'single_host_origin'}
+        />
     </div>
   );
 }
